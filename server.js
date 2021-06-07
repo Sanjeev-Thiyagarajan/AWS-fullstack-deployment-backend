ae84e08642ab7a43e0d209af91318284073d9fc6
Sanjeev Thiyagarajan <sanjeevkt720@gmail.com>
2021-06-07 01:04:36 -0400

    adsf

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get("/api/v1/test", (req, res) => {
   });
 });
 
-// Get all Restaurants
+// Get all Restaurantsfs
 app.get("/api/v1/restaurants", async (req, res) => {
   try {
     //const results = await db.query("select * from restaurants");
